25128b7efbb62f25cfcf4e8dd9175580625d1edc
Amit Singh <https://github.com/AutomationAmitSingh/JavaPluralExamples.git>
2021-09-23 22:48:43 +0530

    dependency also added in POM

--- src/test/java/org/example/Post_Org_Json_Test.java
+++ src/test/java/org/example/Post_Org_Json_Test.java
@@ -1,6 +1,31 @@
 package org.example;
 
-public class Post_Orgjson_Test {
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.json.simple.JSONObject;
 
-    
+import static com.jayway.restassured.RestAssured.*;
+
+public class Post_Org_Json_Test {
+
+    public static void main(String[] args) {
+
+        JSONObject basicInfo = new JSONObject();
+        basicInfo.put("firstName","Sumit");
+        basicInfo.put("lastName","Singh");
+        basicInfo.put("age","21");
+        basicInfo.put("id","5");
+
+        Response res = given()
+                .contentType(ContentType.JSON)
+                .body(basicInfo.toString())
+                .when()
+                .post("http://localhost:3000/friends");
+
+        System.out.println("Status code: "+res.getStatusCode());
+        System.out.println("Data is:");
+        System.out.println(res.asString());
+
+
+    }
 }
